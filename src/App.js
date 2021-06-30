20fd6f444126bfcba7c2bb140a4266d0795c4acb
ianfleming464 <60020259+ianfleming464@users.noreply.github.com>
2021-07-01 00:17:55 +0200

    Very first iteration: simply connect and display some API info.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
-import React, { Component } from "react";
-import "./index.css";
+import React from "react";
+import ReactDom from "react-dom";
+import axios from "axios";
 
-class App extends Component {
+export default class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			data: []
+		};
+	}
+
+	async componentDidMount() {
+		const response = await axios.get("https://api.corona-zahlen.org/germany");
+		this.setState({ data: response.data });
+	}
+
 	render() {
+		const { cases, deaths } = this.state.data;
 		return (
-			<div className='App'>
-				<p>Hi.</p>
-			</div>
+			<>
+				<div className='App'>
+					<h1>First data test:</h1>
+					<h4>Cases: {cases}</h4>
+					<h4>Deaths: {deaths}</h4>
+				</div>
+			</>
 		);
 	}
 }
-
-export default App;
